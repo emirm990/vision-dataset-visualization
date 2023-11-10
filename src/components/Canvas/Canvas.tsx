9d65cefed11c6a61a9feff4ac59abf26ae6a6784
Emir Mekić <emirmekic123@gmail.com>
2023-11-10 10:46:40 +0100

    add zoom and text to lines

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,19 +1,25 @@
 import { TestItem } from '@/types/testdata'
 import { useEffect, useRef } from 'react'
 import { fabric } from 'fabric' // v5
+import type { Canvas as FabricCanvasType } from 'fabric/fabric-impl'
 
 type Props = {
   item: TestItem,
   imagePath: string,
 }
 
+type FabricCanvas = FabricCanvasType & {
+  isDragging: boolean,
+  lastPosY: number,
+  lastPosX: number,
+}
 export default function Canvas(props: Props){
   const {
     item,
     imagePath,
   } = props
 
-  const fabricRef = useRef<fabric.Canvas | null>(null)
+  const fabricRef = useRef<FabricCanvas | null>(null)
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
   const getWidth = () => {
@@ -22,7 +28,10 @@ export default function Canvas(props: Props){
 
   useEffect(() => {
     const initFabric = () => {
-      fabricRef.current = new fabric.Canvas(canvasRef.current)
+      fabricRef.current = new fabric.Canvas(canvasRef.current, {
+        perPixelTargetFind: true,
+        imageSmoothingEnabled: false,
+      }) as FabricCanvas
       fabricRef.current.width = getWidth()
     }
 
@@ -36,10 +45,80 @@ export default function Canvas(props: Props){
       if (fabricRef.current) {
 
         fabricRef.current.setDimensions({width: getWidth(), height: 600})
-        fabricRef.current.setViewportTransform([1, 0, 0, 1, 0, 0])
       }
     }
 
+    fabricRef.current?.on('mouse:down', function(opt) {
+      const evt = opt.e
+      const canvas = fabricRef.current
+      if (!canvas) {
+        return
+      }
+
+      if (evt.altKey === true) {
+        canvas.isDragging = true
+        canvas.selection = false
+        canvas.lastPosX = evt.clientX
+        canvas.lastPosY = evt.clientY
+      }
+    })
+
+    fabricRef.current?.on('mouse:move', function(opt) {
+      const canvas = fabricRef.current
+      if (!canvas) {
+        return
+      }
+
+      if (canvas.isDragging) {
+        const e = opt.e
+        const vpt = canvas.viewportTransform
+        if (vpt) {
+          vpt[4] += e.clientX - canvas.lastPosX
+          vpt[5] += e.clientY - canvas.lastPosY
+          canvas.requestRenderAll()
+          canvas.lastPosX = e.clientX
+          canvas.lastPosY = e.clientY
+        }
+      }
+    })
+  
+    fabricRef.current?.on('mouse:up', function() {
+      // on mouse up we want to recalculate new interaction
+      // for all objects, so we call setViewportTransform
+      const canvas = fabricRef.current
+      if (!canvas) {
+        return
+      }
+      const vpt = canvas.viewportTransform
+      if (vpt) {
+        canvas.setViewportTransform(vpt)
+        canvas.isDragging = false
+        canvas.selection = true
+      }
+    })
+    
+    fabricRef.current?.on('mouse:wheel', function(opt) {
+      const canvas = fabricRef.current
+      const delta = opt.e.deltaY
+
+      if (!canvas) {
+        return
+      }
+
+      let zoom = canvas.getZoom()
+
+      zoom *= 0.999 ** delta
+      if (zoom > 20) zoom = 20
+      if (zoom < 0.01) zoom = 0.01
+      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom)
+
+      opt.e.preventDefault()
+      opt.e.stopPropagation()
+    })
+          
+    fabricRef.current?.on('mouse:over', function(opt) {
+      console.log(opt.target)
+    })
 
     onResize()
     window.addEventListener('resize', onResize)
@@ -52,64 +131,44 @@ export default function Canvas(props: Props){
 
   useEffect(() => {
     if (imagePath) {
-      const addLine = (x: number, y: number, stroke?: string) => {
+      const addLine = (x: number, y: number, stroke?: string, label?: string) => {
         const line = new fabric.Line([x, 0, x, y], {
           strokeWidth: 10,
           stroke: stroke || 'red'
         })
-
-        return line
+        const text = new fabric.Text(`${label}: ${x}px`, {
+          textAlign: 'center',
+          stroke: stroke,
+          fill: stroke,
+          backgroundColor: 'white',
+          padding: 50,
+          originX: 'center',
+          originY: 'center',
+          left: x,
+          top: y + 50,
+        })
+        line.lockMovementY = true
+        const lineGroup = new fabric.Group([line, text])
+        return lineGroup
       }
 
       const addImage = () => {
         fabric.Image.fromURL(imagePath, function(oImg) {
-          const foil_start = addLine(item.result.foil_start, item.height)
-          const foil_end = addLine(item.result.foil_end, item.height)
+          const foil_start = addLine(item.result.foil_start, item.height, 'red', 'result.foil_start')
+          const foil_end = addLine(item.result.foil_end, item.height, 'red', 'result.foil_end')
           const coatingLineSections = item.result.coating_line_sections.flatMap((section) => {
             const result = []
             result.push(
-              addLine(section.coating_start, item.height, 'green'),
-              addLine(section.coating_end, item.height, 'green'),
-              addLine(section.foil_start, item.height, 'green'),
-              addLine(section.foil_end, item.height, 'green')
+              addLine(section.coating_start, item.height, 'green', 'section.coating_start'),
+              addLine(section.coating_end, item.height, 'green', 'section.coating_end'),
+              addLine(section.foil_start, item.height, 'green', 'section.foil_start'),
+              addLine(section.foil_end, item.height, 'green', 'section.foil_end')
             )
 
             return result
           })
-          const lineGroup = new fabric.Group([foil_start, foil_end, ...coatingLineSections])
-
-          const group = new fabric.Group([ oImg, lineGroup ], {
-            left: 0,
-            top: 0,
-            angle: 0,
-          })
-          fabricRef.current?.add(group)
-
-          // group.on('selected', () => {
-          //   fabricRef.current?.setZoom(1)
-          // })
-          
-          // group.on('selected', () => {
-          //   const items = group.getObjects();
-          //   group.destroy();
-          //   fabricRef.current?.remove(group);
-          //   items.forEach(function(item) {
-          //     item.set('dirty', true);
-          //   })
-          //   fabricRef.current?.add.apply(fabricRef.current, items);
-          //   fabricRef.current?.renderAll();
-          // })
-
-          // lineGroup.on('selected', () => {
-          //   const items = lineGroup.getObjects();
-          //   lineGroup.destroy();
-          //   fabricRef.current?.remove(lineGroup);
-          //   items.forEach(function(item) {
-          //     item.set('dirty', true);
-          //   })
-          //   fabricRef.current?.add.apply(fabricRef.current, items);
-          //   fabricRef.current?.renderAll();
-          // })
+          oImg.selectable = false
+          fabricRef.current?.add(...[oImg, foil_start, foil_end, ...coatingLineSections])
         })
       }
       
@@ -119,8 +178,8 @@ export default function Canvas(props: Props){
 
   useEffect(() => {
     if (fabricRef.current) {
-      // const ratio = fabricRef.current.getWidth() / item.width
-      fabricRef.current.setZoom(1)
+      const ratio = fabricRef.current.getWidth() / item.width
+      fabricRef.current.setZoom(ratio)
     }
   }, [item])
 
